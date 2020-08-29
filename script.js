e5487963cb139fa2128d8eca67d401b5c49b93e9
Stan S <stan.shm@gmail.com>
2020-08-29 18:21:58 -0500

    adding script

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-const offer = document.querySelector("#offer");
-console.log(offer)
+
+const offersText = [
+    {
+        id:0,
+        model: '2021 BMW X7',
+        type:'Lease for',
+        dollarAmount:'479',
+        lenght: 'for 36 mos',
+        das:'$0 Due at Signing',
+        inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/x3/'
+    },
+    {
+        id:1,
+        model: '2021 BMW 3 Series',
+        type:'Lease for',
+        dollarAmount:'249',
+        lenght: 'for 36 mos',
+        das:'$0 Due at Signing',
+        inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/x3/'
+    },
+    {
+        id:2,
+        model: '2021 BMW X3',
+        type:'Lease for',
+        dollarAmount:'399',
+        lenght: 'for 36 mos',
+        das:'$0 Due at Signing',
+        inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/x3/'
+    }
+]
+
+console.log(offersText[1].dollarAmount)
+
+const offerIndex = document.querySelector('.carousel-indicators li').classList;
+console.log(offerIndex)
+/* const offer = Array.from(document.querySelectorAll('#offer li'));
+
+console.log(offer.textContent) */
+
+
+
+
 
 function changeOffer() {
+
+
+    const offer = Array.from(document.querySelectorAll('#offer li'));
+    
+    console.log(offer.length)
    
+  
    
 }
