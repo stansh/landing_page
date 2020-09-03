b1a10dad4b7678adcf8ed14251b8d863169c7be3
Stan S <stan.shm@gmail.com>
2020-09-03 00:08:34 -0500

    videos link, prev button doesnt work

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const offersData = [
         model: '2021 BMW 3 Series',
         type:'Lease for',
         dollarAmount:'249',
-        length: 'for 36 mos',
+        length: 'for 24 mos',
         das:'$0 Due at Signing',
         inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/3-series/',
         videoSrc:'videos/3series.mp4'
@@ -33,36 +33,46 @@ const offersData = [
         videoSrc:'videos/X3.mp4'
     }
 ]
-
 window.onload = function getVideoLinks() {
-    const videoSlides = Array.from(document.querySelectorAll('video source')); 
+    const videoSlides = Array.from(document.querySelectorAll('video ')); 
     for (let i = 0; i < offersData.length; i++) {
-    /* videoSlides[i].src = offersData[i].videoSrc; */
     videoSlides[i].setAttribute('src', offersData[i].videoSrc);
     }
  }
 
 
 
-
-/* = offersData[i].videoSrc); */
-
-
-
-
 function Next (prop) {
     index = index + 1;
-    index === 3 ? index = 0 : index;
+    index === offersData.length ? index = 0 : index;
     const offer = Array.from(document.querySelectorAll('#offer li'));
     let buttonLink = document.querySelector('#offer a');
-    let video = document.querySelector('video source').getAttribute('src');
     offer[0].innerHTML = offersData [index].model;
     offer[1].innerHTML = offersData [index].type;
     offer[2].innerHTML = `<span id='dollar'>$</span>`+ offersData [index].dollarAmount;
     offer[3].innerHTML = offersData [index].length;
     offer[4].innerHTML = offersData [index].das;
-    buttonLink.href = offersData [index].inventoryLink;  
-    video.src = offersData [index].videoSrc;
+    buttonLink.href = offersData [index].inventoryLink; 
+    
+}
+
+function Prev (prop) {
+    console.log(index)
+    
+    index === 0 ? index = 2 : '';
+    console.log(index)
+    
+ 
+
+    const offer = Array.from(document.querySelectorAll('#offer li'));
+    let buttonLink = document.querySelector('#offer a');
+    offer[0].innerHTML = offersData [index].model;
+    offer[1].innerHTML = offersData [index].type;
+    offer[2].innerHTML = `<span id='dollar'>$</span>`+ offersData [index].dollarAmount;
+    offer[3].innerHTML = offersData [index].length;
+    offer[4].innerHTML = offersData [index].das;
+    buttonLink.href = offersData [index].inventoryLink; 
+    
 }
 
  
