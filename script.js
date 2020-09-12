5464f293b281d17b72f0d375c6cd421c9d10d3b9
Stan S <stan.shm@gmail.com>
2020-09-12 15:35:00 -0500

    other changes

--- script.js
+++ script.js
@@ -4,68 +4,59 @@ let offerIndex = 0;
 const offersData = [
     {
         id:0,
-        model: '2021 BMW X7',
-        type:'Lease for',
-        dollarAmount:'479',
-        length: 'for 36 mos',
-        das:'$0 Due at Signing',
-        inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/x7/',
-        videoSrc:'videos/X7.mp4'
+        model: "2021 BMW X7",
+        type:"Lease for",
+        dollarAmount:"479",
+        length: "for 36 mos",
+        das:"$0 Due at Signing",
+        inventoryLink:"https://www.bmwofminnetonka.com/new-vehicles/x7/",
+        videoSrc:"videos/X7.mp4"
     },
     {
         id:1,
-        model: '2021 BMW 3 Series',
-        type:'Lease for',
-        dollarAmount:'249',
-        length: 'for 24 mos',
-        das:'$0 Due at Signing',
-        inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/3-series/',
-        videoSrc:'videos/3series.mp4'
+        model: "2021 BMW 3 Series",
+        type:"Lease for",
+        dollarAmount:"249",
+        length: "for 24 mos",
+        das:"$0 Due at Signing",
+        inventoryLink:"https://www.bmwofminnetonka.com/new-vehicles/3-series/",
+        videoSrc:"videos/3series.mp4"
     },
     {
         id:2,
-        model: '2021 BMW X3',
-        type:'Lease for',
-        dollarAmount:'399',
-        length: 'for 36 mos',
-        das:'$0 Due at Signing',
-        inventoryLink:'https://www.bmwofminnetonka.com/new-vehicles/x3/',
-        videoSrc:'videos/X3.mp4'
+        model: "2021 BMW X3",
+        type:"Lease for",
+        dollarAmount:"399",
+        length: "for 36 mos",
+        das:"$0 Due at Signing",
+        inventoryLink:"https://www.bmwofminnetonka.com/new-vehicles/x3/",
+        videoSrc:"videos/X3.mp4"
     }
 ]
 window.onload = function getVideoLinks() {
-    const videoSlides = Array.from(document.querySelectorAll('video')); 
+    const videoSlides = Array.from(document.querySelectorAll("video")); 
     for (let i = 0; i < offersData.length; i++) {
-        videoSlides[i].setAttribute('src', offersData[i].videoSrc);
-        videoSlides[i].setAttribute('id', offersData[i].id);
+        videoSlides[i].setAttribute("src", offersData[i].videoSrc);
+        videoSlides[i].setAttribute("id", offersData[i].id);
     }   
     videoSlides[0].autoplay = true;
  } 
 
 
-
-
-
-
-
-
-
-function Next (props) {
-    
-    
+function Next (props) {  
     offerIndex = offerIndex + 1;
     offerIndex === offersData.length ? offerIndex = 0 : offerIndex;
-    const offer = Array.from(document.querySelectorAll('#offer li'));
-    let buttonLink = document.querySelector('#offer a');
+    const offer = Array.from(document.querySelectorAll("#offer li"));
+    let buttonLink = document.querySelector("#offer a");
     offer[0].innerHTML = offersData [offerIndex].model;
     offer[1].innerHTML = offersData [offerIndex].type;
-    offer[2].innerHTML = `<span id='dollar'>$</span>`+ offersData [offerIndex].dollarAmount;
+    offer[2].innerHTML = `<span id="dollar">$</span>`+ offersData [offerIndex].dollarAmount;
     offer[3].innerHTML = offersData [offerIndex].length;
     offer[4].innerHTML = offersData [offerIndex].das;
     buttonLink.href = offersData [offerIndex].inventoryLink; 
-    const videos = Array.from(document.querySelectorAll('video'));
-    videos.forEach(video => video.pause())  
-    videos[offerIndex].play();   
+    const videos = Array.from(document.querySelectorAll("video"));
+    videos.forEach(video => video.pause());  
+    videos[offerIndex].play(); 
 }
 
 function Prev (props) {
@@ -78,17 +69,18 @@ function Prev (props) {
          break;
      }
      
-    const offer = Array.from(document.querySelectorAll('#offer li'));
-    let buttonLink = document.querySelector('#offer a');
+    const offer = Array.from(document.querySelectorAll("#offer li"));
+    let buttonLink = document.querySelector("#offer a");
     offer[0].innerHTML = offersData [offerIndex].model;
     offer[1].innerHTML = offersData [offerIndex].type;
-    offer[2].innerHTML = `<span id='dollar'>$</span>`+ offersData [offerIndex].dollarAmount;
+    offer[2].innerHTML = `<span id="dollar">$</span>`+ offersData [offerIndex].dollarAmount;
     offer[3].innerHTML = offersData [offerIndex].length;
     offer[4].innerHTML = offersData [offerIndex].das;
     buttonLink.href = offersData [offerIndex].inventoryLink; 
-    const videos = Array.from(document.querySelectorAll('video'));
+    const videos = Array.from(document.querySelectorAll("video"));
     videos.forEach(video => video.pause())  
-    videos[offerIndex].play();  
+    videos[offerIndex].play();   
+
     
 }
 
